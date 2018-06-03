a531c62759890f56745e348085ad80923fa1e6af
Jhhames <Fjhhames@gmail.com>
2018-06-03 23:23:14 +0100

    Introduction to API writing, simple login API ,Takes form-data then compares to a string

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,12 +6,50 @@ defined('BASEPATH') or exit('No direct scripts allowed beyond this point');
 		public function __construct()
 		{
 			parent::__construct();
-			$this->load->helper(array('file', 'url', 'form'));
+			$this->load->helper(array('file', 'url', 'form','json_output_helper'));
+			$this->load->library('form_validation');
+			$this->load->model('Main');
 		}
 
 		public function index()
 		{
 			$this->load->view('index');
 		}
+
+		public function endpoint()
+		{
+			$method = $_SERVER['REQUEST_METHOD'];
+			if ($method != 'POST')
+			{
+				return json_output(405, array('status' => 405, 'message' => 'Bad Request'));
+			}
+			else
+			{	
+				$this->form_validation->set_rules('username','Username','required');
+				$this->form_validation->set_rules('password','Password','required');
+				if( $this->form_validation->run() == false){
+					$message = 'Form validation error';
+					return json_output(400, array('status' => 400, 'message' => $message, 'error' => validation_errors()));
+				}
+				else{
+					$username = $this->input->post('username');
+					$password = $this->input->post('password');
+					if($username== 'james' && $password == 'password'):
+						$status = 200;
+						$response = 'Login Successful';
+						$message = 'Welcome';
+					else:
+						$status = 400;
+						$response = 'Invalid Username or password';
+						$message = 'Try again';						
+					endif;				
+					
+					return json_output($status, array('status' => $status, 'message' => $message, 'response' => $response));
+				}
+
+			}
+		}
+	
+	
 	}
 ?>
---
+++ application/models/Main.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('no direct blah blah blah');
+    class Main extends CI_MODEL
+    {
+        function __construct()
+        {
+            parent::__construct();
+            $this->load->database();
+
+        }
+        public function checklogin()
+        {
+            
+        }
+    }
+?>
